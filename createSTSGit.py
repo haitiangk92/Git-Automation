27f32ca95e7d6c3d694381423ff9c7c53ff8836f
haitiangk92 <haitiangk92@gmail.com>
2020-12-18 21:43:18 -0500

    first commit

---
+++ createSTSGit.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+os.system("pwd >> temp.txt")
+
+repoName = open("temp.txt", "r").readlines()[0].split("/")[-1].strip()
+createRepo = f"curl -H \"Authorization: token $GIT_TOKEN\" https://api.github.com/user/repos -d '{{\"name\": \"{repoName}\"}}'"
+os.system("rm temp.txt")
+os.system(createRepo)
+
+time.sleep(1)
+print("\nRepo Created\n")
+
+os.system("git init")
+print("\nGit Initiated\n")
+
+with open(".gitignore", 'w') as ignore:
+    ignore.write(".classpath\n.project\n/bin/*")
+print("\n .ignore File Created\n")
+
+os.system("git add .")
+print("\nFiles added\n")
+time.sleep(1)
+
+os.system(f"git remote add origin https://github.com/haitiangk92/{repoName}.git")
+print("\nRepo Linked to Directory\n")
+time.sleep(1)
+
+os.system('git commit -m "first commit"')
+print("\nFiles Commited\n")
+time.sleep(1)
+
+os.system("git push -u origin master")
+print("\n\n***Complete***\n\n")
---
+++ updateGit.py
@@ -0,0 +1,21 @@
+import os
+import time
+import sys
+
+try:
+    commitMessage = sys.argv[1]
+    print(f"Committing with message: {commitMessage}")
+except:
+    print("\nNo Commit Messsage Detected\n".upper())
+    quit()
+
+os.system("git add .")
+print("\nFiles Added\n")
+time.sleep(1)
+
+os.system(f'git commit -m "{commitMessage}"')
+print("\nFiles Committed\n")
+time.sleep(1)
+
+os.system("git push -u origin master")
+print("\n\n**Complete***\n\n")
